45b6004411bddde0af70a0e6ba3e8fd3865c00e0
Rashed2025 <eng.rmgh7@gmail.com>
2025-04-28 11:52:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Start FastAPI backend
-uvicorn backend:app --host 0.0.0.0 --port 8000 &
+uvicorn backend:app --host 0.0.0.0 --port 8000 --reload &
+
+# Wait a few seconds to make sure backend is ready
+sleep 15
 
 # Start Streamlit frontend
 streamlit run chatbot.py --server.port 8501 --server.address 0.0.0.0
