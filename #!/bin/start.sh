0511750d0fe10e0fee73cb167c22da728e1ae7c8
Rashed2025 <eng.rmgh7@gmail.com>
2025-04-28 11:47:35 +0300

    Delete #!/bin directory

--- #!/bin/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start FastAPI backend
-uvicorn backend:app --host 0.0.0.0 --port 8000 &
-
-# Start Streamlit frontend
-streamlit run chatbot.py --server.port 8501 --server.address 0.0.0.0
